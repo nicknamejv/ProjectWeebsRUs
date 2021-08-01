934823d471b0f40e394f9cc267b5a5718c741138
TevennLewis <tevennlewis@gmail.com>
2021-08-01 16:14:14 -0700

    added show route to video controller

--- controllers/video_controller.js
+++ controllers/video_controller.js
@@ -2,3 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 const { Video } = require('../models');
+
+// NOTE: SHOW route
+router.get('video/:id', (req, res, next) => {
+    Video.findById(req.params.id, (error, foundVideo) => {
+        if (error) {
+            console.log(error);
+            req.error = error;
+            return next();
+        };
+
+        const context = {
+            videos: foundVideo,
+        };
+  
+        return res.render('videos/index', context);
+      });
+    });
+  
