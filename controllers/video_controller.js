445772d3e5d924fbf38de8de21444cf715c51d03
nicknamejv <jonvercabral@yahoo.com>
2021-08-04 16:55:57 -0700

    removed coded from show route

--- controllers/video_controller.js
+++ controllers/video_controller.js
@@ -32,8 +32,6 @@ router.get('/:id', async (req, res, next) => {
             comments: allComment,
         };
 
-        console.log(`${allComment} ========`);
-
         return res.render('videos/show', context);
         
     } catch (error) {
